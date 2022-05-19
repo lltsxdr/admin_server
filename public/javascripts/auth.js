80988d9cecbe7abe9f1d8b8c113812042400baa3
lltsxdr <103183214+lltsxdr@users.noreply.github.com>
2022-05-19 14:15:37 +0800

    token 鉴权

---
+++ public/javascripts/auth.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken')
+const TOKEN_SECRET = 'express'
+
+const tokenArr = new Set()
+
+const auth = (req, res, next) => {
+	const account = jwt.decode(req.headers.authorization, TOKEN_SECRET)
+
+	const isLogin = tokenArr.has(`${account}@${req.headers.authorization}` ?? '')
+
+	if (!isLogin) {
+		res.json({
+			code: -1,
+			msg: '请登录',
+			data: null
+		})
+	} else {
+		next()
+	}
+}
+
+module.exports = {
+	auth,
+	TOKEN_SECRET,
+	tokenArr
+}
--- routes/users.js
+++ routes/users.js
@@ -1,41 +1,73 @@
 const express = require('express')
 const jwt = require('jsonwebtoken')
 const router = express.Router()
-
-const TOKEN_SECRET = 'express'
-
-const tokenObj = {}
+const { auth, TOKEN_SECRET, tokenArr } = require('../public/javascripts/auth')
 
 /* GET users listing. */
 router.post('/login', function (req, res) {
-	if (req.body.username === '123123' && req.body.password === '123123') {
+	if (!req.body.username || !req.body.password) {
+		return res.json({
+			code: 1,
+			msg: '非法参数',
+			data: null
+		})
+	}
+
+	const { username, password } = req.body
+
+	if (username !== '123123' || password !== '123123') {
+		res.json({
+			code: 1,
+			msg: '账号或密码错误',
+			data: null
+		})
+	} else {
 		const userAccount = Object.values(req.body).join('&')
 
 		const token = jwt.sign(userAccount, TOKEN_SECRET)
 
-		tokenObj[userAccount] = token
+		tokenArr.add(userAccount + '@' + token)
 
 		res.json({
 			code: 200,
 			msg: 'success',
-			data: token
-		})
-	} else {
-		res.json({
-			code: 1,
-			msg: 'fail',
-			data: null
+			data: {
+				token,
+				role: 1
+			}
 		})
 	}
 })
 
-router.get('/validToken', (req, res) => {
-	console.log(req.query, tokenObj)
+router.get('/info', auth, (req, res) => {
+	const account = jwt.decode(req.headers.authorization, TOKEN_SECRET)
+
+	const [ username ] = account.split('&')
 
 	res.json({
-		code: 1,
-		msg: 'fail',
-		data: null
+		code: 200,
+		msg: 'success',
+		data: {
+			username,
+			role: 1,
+			age: 25,
+			male: 'Female',
+			job: 'FrontEnd Developer'
+		}
+	})
+})
+
+router.post('/modify', auth, (req, res) => {
+
+})
+
+router.get('/validToken', auth, (req, res) => {
+	res.json({
+		code: 200,
+		msg: 'success',
+		data: {
+			auth: 1
+		}
 	})
 })
 
